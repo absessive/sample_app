af2b8102418c66569df70652a107bc9de5c91c0b
Ajit Chakrapani <absessive@gmail.com>
2013-02-09 20:27:40 -0500

    fixed broken Paren

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name: "Example User", email: "user@example.com")
+  before { @user = User.new(name: "Example User", email: "user@example.com") }
   
   subject { @user }
   
